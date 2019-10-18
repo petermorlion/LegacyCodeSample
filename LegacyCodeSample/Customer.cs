0489896f9bd65563a2eca187093750a4f045e1b3
Peter Morlion <peter.morlion@gmail.com>
2019-10-18 12:47:37 +0200

    Initial commit

---
+++ LegacyCodeSample/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LegacyCodeSample
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public bool IsGoldMember { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        public bool IsEmployee()
+        {
+            var lines = File.ReadAllLines("database.txt");
+            var person = lines.SingleOrDefault(x => x.StartsWith($"{Id};"));
+            if (person == null)
+            {
+                throw new Exception("Employee not found");
+            }
+
+            var isEmployee = person.Split(';')[1];
+            return isEmployee == "1";
+        }
+    }
+}
---
+++ LegacyCodeSample/DiscountCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LegacyCodeSample
+{
+    public class DiscountCalculator
+    {
+        public double CalculateDiscount(Customer customer)
+        {
+            if (!customer.IsGoldMember && customer.Birthdate.Month == DateTime.UtcNow.Month && customer.Birthdate.Day == DateTime.UtcNow.Day)
+            {
+                return 40;
+            }
+
+            if (customer.IsGoldMember)
+            {
+                if (DateTime.UtcNow.Year - customer.Birthdate.Year > 40)
+                {
+                    return 30;
+                }
+
+                if (customer.IsEmployee())
+                {
+                    if (DateTime.UtcNow.Year - customer.Birthdate.Year > 40)
+                    {
+                        return 20;
+                    }
+                    else
+                    {
+                        return 15;
+                    }
+                }
+
+                if (customer.Birthdate.Month == DateTime.UtcNow.Month && customer.Birthdate.Day == DateTime.UtcNow.Day)
+                {
+                    return 40;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
